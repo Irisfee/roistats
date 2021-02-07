86fa70a1102778d28636615b724d51ccab3e4ad5
Yufei <fay.z@163.com>
2021-02-06 22:10:51 -0800

    two_sample_t_test

--- R/t_test.R
+++ R/t_test.R
@@ -19,3 +19,26 @@ t_test_one_sample <- function(data, x, mu, p_adjust = "bonferroni") {
     dplyr::bind_cols(adjust_p_df)
   return(t_results)
 }
+
+
+
+t_test_two_sample <- function(data, x, y, paired = FALSE, p_adjust = "bonferroni") {
+  if (!dplyr::is.grouped_df(data))  stop('Data must be a grouped data frame.')
+  t_results <- data %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      ttest = purrr::map(data, ~ stats::t.test(.x[[x]] ~ .x[[y]], paired = paired)),
+      tvalue = purrr::map_dbl(ttest, ~ magrittr::extract2(.x, 1)),
+      df = purrr::map_dbl(ttest, ~ magrittr::extract2(.x, 2)),
+      p = purrr::map_dbl(ttest, ~ magrittr::extract2(.x, 3))
+    ) %>%
+    dplyr::select(-ttest, -data)
+
+  adjust_p_df <- purrr::map_dfc(p_adjust, ~ p_adjust_func(method = .x, data = t_results))
+  # c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY",
+  #   "fdr", "none")
+  t_results <- t_results %>%
+    dplyr::bind_cols(adjust_p_df)
+  return(t_results)
+}
+
--- tests/testthat/test-t_test_one_sample.R
+++ tests/testthat/test-t_test_one_sample.R
@@ -1,3 +1,4 @@
 test_that("t-test-one-sample output", {
-  expect_length(t_test_one_sample(test, "color_index", mu = 0), 5)
+  expect_length(t_test_one_sample(color_index, "color_index", mu = 0), 5)
+  expect_equal(NROW(na.omit(t_test_one_sample(color_index, "color_index", mu = 0))), 8)
 })
